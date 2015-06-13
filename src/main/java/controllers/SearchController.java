1347584af6b235e7e8b5b33c37f880534df7b9d4
ADelavina <nw.delo@gmail.com>
2015-06-13 19:43:31 -0300

    Refactor webapp

--- src/main/java/controllers/SearchController.java
+++ src/main/java/controllers/SearchController.java
@@ -5,18 +5,17 @@ import java.util.List;
 import models.OpResult;
 import models.ResultDocument;
 
-
-
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import services.ElasticSearch;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SearchController {
 	
@@ -33,4 +32,9 @@ public class SearchController {
 		
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(SearchController.class);
+	//	ElasticSearch.BuildElasticSearch();
+	}
+	
 }
